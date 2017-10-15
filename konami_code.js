f62e63d5cf892afef9edfe9b72d00bea660b1bc7
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-10-15 20:46:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(i) {
+function init('keydown') {
   // Write your JavaScript code inside the init() function
-  alert('keydown')
 
 }
